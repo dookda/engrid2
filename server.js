0078ff87274874ab5027d2c2e705719cc67ee49e
dookda <sakda.homhuan@gmail.com>
2021-09-02 09:08:37 +0700

    add to docker

--- server.js
+++ server.js
@@ -16,4 +16,13 @@ app.use('/', express.static('www'))
 const hp = require('./service/hotspot');
 app.use(hp);
 
+const nodeEnv = process.env.NODE_ENV || 'development'
+
+app.get("/api", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        nodeEnv
+    })
+})
+
 
